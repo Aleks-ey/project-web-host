0c644c44893bf6c4f2b28ace1082e98920b82a4e
Aleksey Huhua <Akhukhua@gmail.com>
2022-04-16 18:23:42 -0600

    cors fix

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express()
 const cookieParser = require('cookie-parser')
 const cors = require("cors")
 require('dotenv').config()
-app.use(cors({origin: 'http://localhost:3000', credentials: true}))
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 //const {SERVER_PORT} = process.env

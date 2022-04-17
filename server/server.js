ac4662512287e4360e1279ac213d83147c9a25db
Aleksey Huhua <Akhukhua@gmail.com>
2022-04-16 18:53:47 -0600

    path change 5th time

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const app = express()
 const cookieParser = require('cookie-parser')
 const cors = require("cors")
 require('dotenv').config()
-app.use(cors())
+app.use(cors({origin:"*"}))
 app.use(express.json())
 app.use(cookieParser())
 //const {SERVER_PORT} = process.env
 const port = process.env.PORT || 5000;
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, '/build/index.html')));
+app.use(express.static(path.join(__dirname, '../build/index.html')));
 
 app.get('/', function (req,res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));

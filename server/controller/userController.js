5daab39721630646f68e698e7de6ba073aaf01a1
Aleksey Huhua <Akhukhua@gmail.com>
2022-04-16 19:32:44 -0600

    changed connection_string to database_url

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 var bcrypt = require('bcryptjs');
-const { CONNECTION_STRING } = process.env;
+const { DATABASE_URL } = process.env;
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(CONNECTION_STRING, {
+const sequelize = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
     ssl: {

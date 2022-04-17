a477873a0f094cd91aa689a05e58719304ff1ef3
Aleksey Huhua <Akhukhua@gmail.com>
2022-04-16 19:17:48 -0600

    path change for app.use

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(cookieParser())
 const port = process.env.PORT || 5000;
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, '../build/index.html')));
+app.use(express.static(path.join(__dirname, '../')));
 
 app.get('/', function (req,res) {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
